9479c763c0ed05078f9da334550f3a5dee8b8624
Menos  Velasco jr <menozvelasco1@gmail.com>
2020-08-30 16:04:58 -0700

    all test pass decode_bits

--- morse.py
+++ morse.py
@@ -12,9 +12,9 @@ Pause between words – is 7 time units long.
 __author__ = """ manuel velasco  John(SE Coach)"""
 
 from morse_dict import MORSE_2_ASCII
-import re
 # hey_jude_morse = ".... . -.--   .--- ..- -.. ."
-# ['....', ' ', '.', ' ', '-.--', '   ', '.---', ' ', '..-', ' ', '-..', ' ', '.']
+""" ['....', ' ', '.', ' ', '-.--', '   ',
+ '.---', ' ', '..-', ' ', '-..', ' ', '.'] """
 # MORSE_2_ASCII.get('....') == > 'H'
 
 
@@ -31,12 +31,41 @@ def decode_morse(morse):
 
 
 def decode_bits(bits):
-    pass
+    bits = bits.strip('0')
+
+    two_bits = min([len(bit)
+                    for bit in bits.split("1") + bits.split("0") if bit])
+
+    return bits.replace(
+        '111' * two_bits, '-'
+    ).replace(
+        '1' * two_bits, '.'
+    ).replace(
+        "0000000" * two_bits, '   '
+    ).replace(
+        '000' * two_bits, ' '
+    ).replace(
+        '0' * two_bits, ''
+    )
+
+    # for n, bit in enumerate(order):
+    #     if order[n:n+5] == '':
+    #         result += '-'
+    #     elif order[n] == '':
+    #         result += '.'
+    #     elif order[n] == '00':
+    #         result += ' '
+    #     elif order[n] == '0000000':
+    #         result += '  '
+    #     else:
+    #         result += '  '
+    # print(result)
+    # return result
 
 
 if __name__ == '__main__':
     hey_jude_morse = ".... . -.--   .--- ..- -.. ."
-    hey_jude_bits = "11001100110011000000110000001111110011001111110011111100000000000000000000011001111110011111100111111000000110011001111110000001111110011001100000011"  # noqa
+    hey_jude_bits = "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011"  # noqa
 
     # Be sure to run all included unit tests, not just this one.
     print("Morse Code decoder test")
